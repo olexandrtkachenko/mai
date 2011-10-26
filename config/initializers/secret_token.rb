bee2a8c7259013847693e3924d9670a69f8b4986
Oleksandr M. Tkachenko <tkachenko.ck@gmail.com>
2011-10-26 14:21:33 +0300

    Imitial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mai::Application.config.secret_token = '98bd30e6ebe0ee4e669a45176bbd4af593d9c65ae942fe41deb2491061d21ee8ae31e7d98f947f691e4239ea22b3fbffe5306198593bbbb0668c614825ed5f3e'
